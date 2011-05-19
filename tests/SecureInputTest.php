8500aec2c5208b0f9d7ac2a213d9e7b6ef6c3e00
Yasushi Ichikawa <ichikaway@gmail.com>
2011-05-19 11:25:21 +0900

    add test case of clean_key_value method

--- tests/SecureInputTest.php
+++ tests/SecureInputTest.php
@@ -22,8 +22,27 @@ class SecureInputTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals($expect, $result);
 	}
 
+
+	public function  testClean_key_value() {
+		$input = array('a1' => "aa\x00bbb", 'b1' => "bbb\x01\x02\x03ccc", 'c1' => "ccc\x0addd");
+		$expect = array('a1' => "aabbb", 'b1' => 'bbbccc', 'c1' => "ccc\x0addd");
+
+		$result = SecureInput::clean_key_value($input);
+		$this->assertEquals($expect, $result);
+	}
+
+
+	public function  testClean_key_value_with_invalid_key() {
+		$input = array(
+			'a1' => "aa\x00bbb_ok", 'b1' => array('_SERVER'=>'delete', 'normal'=>'ok'), '_GET' => array('id'=>111),
+			'a?e' => 'delete', 'no-o_k' => 'ok', 'onlyValue'
+		);
+		$expect = array('a1' => "aabbb_ok", 'b1' => array('normal'=>'ok'), 'no-o_k' => 'ok', 'onlyValue');
+
+		$result = SecureInput::clean_key_value($input);
+		$this->assertEquals($expect, $result);
+	}
+
+
 }
-
-
-
 ?>
